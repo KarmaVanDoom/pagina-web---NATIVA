7ec6f761a60cc0937d7d1b97160fa33eef2f0d5d
Jairo Castillo <karmacastillo2017@gmail.com>
2026-02-28 18:51:23 -0300

    ultima modificacion nosotros menu

--- main/views.py
+++ main/views.py
@@ -5,3 +5,9 @@ def index(request):
     """Página de inicio."""
     return render(request, 'dashboard/index.html')
 
+
+def nosotros(request):
+    """Página de Nosotros."""
+    return render(request, 'about/nosotros.html')
+
+
